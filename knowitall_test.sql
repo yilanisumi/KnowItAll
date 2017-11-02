f1cb121f1a742fe386529c66a12106b152ca6ec1
Yi Lan <isumi.e.lan@gmail.com>
2017-11-01 18:16:49 -0700

    database script

---
+++ knowitall_test.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2017 at 05:36 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `knowitall_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `search_temp`
+--
+
+CREATE TABLE `search_temp` (
+  `user_id` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `survey_id` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `create_time` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `voter_number` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey`
+--
+
+CREATE TABLE `survey` (
+  `survey_id` varchar(45) NOT NULL,
+  `survey_title` varchar(1000) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `rating_average` double NOT NULL,
+  `create_time` varchar(45) DEFAULT NULL,
+  `survey_tags` varchar(1000) NOT NULL,
+  `voter_number` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey_comments`
+--
+
+CREATE TABLE `survey_comments` (
+  `survey_id` varchar(45) NOT NULL,
+  `comment_string` varchar(1000) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `comment_time` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey_options`
+--
+
+CREATE TABLE `survey_options` (
+  `survey_id` varchar(45) NOT NULL,
+  `option_id` int(45) NOT NULL,
+  `option_string` varchar(1000) NOT NULL,
+  `voter_number` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trending_survey`
+--
+
+CREATE TABLE `trending_survey` (
+  `survey_id` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` varchar(45) NOT NULL,
+  `usc_email` varchar(45) NOT NULL,
+  `usc_id` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `user_name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `usc_email`, `usc_id`, `password`, `user_name`) VALUES
+('1111111111', 'test@usc.edu', '1111111111', 'test', 'testing'),
+('9999999999', 'admin@usc.edu', '9999999999', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_survey`
+--
+
+CREATE TABLE `user_survey` (
+  `user_id` varchar(45) NOT NULL,
+  `survey_id` varchar(45) NOT NULL,
+  `option_id` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `survey`
+--
+ALTER TABLE `survey`
+  ADD PRIMARY KEY (`survey_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `survey_comments`
+--
+ALTER TABLE `survey_comments`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `survey_id` (`survey_id`);
+
+--
+-- Indexes for table `survey_options`
+--
+ALTER TABLE `survey_options`
+  ADD KEY `survey_id` (`survey_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_survey`
+--
+ALTER TABLE `user_survey`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `survey_id` (`survey_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `survey`
+--
+ALTER TABLE `survey`
+  ADD CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `survey_comments`
+--
+ALTER TABLE `survey_comments`
+  ADD CONSTRAINT `survey_comments_ibfk_1` FOREIGN KEY (`survey_id`) REFERENCES `survey` (`survey_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `survey_options`
+--
+ALTER TABLE `survey_options`
+  ADD CONSTRAINT `survey_options_ibfk_1` FOREIGN KEY (`survey_id`) REFERENCES `survey` (`survey_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_survey`
+--
+ALTER TABLE `user_survey`
+  ADD CONSTRAINT `user_survey_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
