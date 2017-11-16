6bad5c3033ebb90763d0430a0a391b00f0242efc
Yi Lan <isumi.e.lan@gmail.com>
2017-11-15 16:17:07 -0800

    some crap

--- www/knowitall/change-password.php
+++ www/knowitall/change-password.php
@@ -1,3 +1,10 @@
+<?php 
+  session_start();
+  if(!isset($_SESSION['id'])){
+    header("Location: login.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -11,18 +18,21 @@
   </head>
   <body>
     <?php include('big-header.php');?>
-    <form class="ui form login-padding" action="" method="post">
+    <div class="ui error message master-center custom-margin-hor custom-margin-bot-small <?php if(empty($failed)){echo "hidden";} ?>">
+      <p>The Information You Entered Was Invalid</p>
+    </div>
+    <form class="ui form login-padding" action="changePasswordHelp.php" method="post">
       <div class="field">
         <label>Old Password</label>
-        <input type="email" name="signupEmail" placeholder="Old Password" required="true">
+        <input type="password" name="oldPassword" placeholder="Old Password" required="true">
       </div>
       <div class="field">
         <label>New Password</label>
-        <input type="text" name="signupID" placeholder="New Password" required="true">
+        <input type="password" name="newPassword" placeholder="New Password" required="true">
       </div>
       <div class="field">
         <label>Confirm New Password</label>
-        <input type="password" name="signupPassword" placeholder="Confirm New Password" required="true">
+        <input type="password" name="confirmPassword" placeholder="Confirm New Password" required="true">
       </div>
       <button class="ui button" type="submit">Change Password</button>
       <a class="signup-link" href="user.php">Cancel</a>
