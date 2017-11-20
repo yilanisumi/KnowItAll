dcfddd0696e178e2429a292d45e92a4dc915d927
Yi Lan <isumi.e.lan@gmail.com>
2017-11-19 18:02:03 -0800

    filter functions

--- www/knowitall/user.php
+++ www/knowitall/user.php
@@ -16,15 +16,7 @@
   $ans = $sql->get_result();
   $urow = $ans->fetch_assoc();
 
-  $sql = $conn->prepare("SELECT * FROM survey WHERE user_id = ?;");
-  $sql->bind_param('s', $uscid);
-  $sql->execute();
-  $ans2 = $sql->get_result();
 
-  $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? ORDER BY action_time DESC;");
-  $sql->bind_param('s', $uscid);
-  $sql->execute();
-  $ans3 = $sql->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -73,7 +65,55 @@
       <div class="equal width row">
         <ul class="column">
           <li class="row header custom-pad-hor"><h4>Activity Log:</h4></li>
-          <?php 
+          <li class="row header custom-pad-hor custom-pad-vert-small">
+            <span class="custom-pad-hor-space">Filter:</span>
+            <?php
+              $filter2 = "";
+              if(!empty($_GET['filter2'])){
+                $filter2 = $_GET['filter2'];
+              }
+            ?>
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter2=".$filter2?>">All</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter2=".$filter2?>&filter1=vote">Vote</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter2=".$filter2?>&filter1=rate">Rate</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter2=".$filter2?>&filter1=create">Create</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter2=".$filter2?>&filter1=comment">Comment</a>
+          </li>
+          <?php
+            if(!empty($_GET['filter1'])){
+              if(strcmp($_GET['filter1'], "vote") == 0){
+                $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? AND action = 0 ORDER BY action_time DESC;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans3 = $sql->get_result();
+              }else if(strcmp($_GET['filter1'], "rate") == 0){
+                $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? AND action = 1 ORDER BY action_time DESC;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans3 = $sql->get_result();
+              }else if(strcmp($_GET['filter1'], "create") == 0){
+                $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? AND action = 2 ORDER BY action_time DESC;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans3 = $sql->get_result();
+              }else if(strcmp($_GET['filter1'], "comment") == 0){
+                $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? AND action = 3 ORDER BY action_time DESC;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans3 = $sql->get_result();
+              }else{
+                $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? ORDER BY action_time DESC;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans3 = $sql->get_result();
+              }
+            }else{
+              $sql = $conn->prepare("SELECT * FROM user_activity WHERE user_id = ? ORDER BY action_time DESC;");
+              $sql->bind_param('s', $uscid);
+              $sql->execute();
+              $ans3 = $sql->get_result();
+            }
+
             for($i = 0; $i < $ans3->num_rows; $i++){
               $arow = $ans3->fetch_assoc();
               include('activity-card.php');
@@ -82,7 +122,42 @@
         </ul>
         <ul class="column">
           <li class="row header custom-pad-hor"><h4>My Surveys:</h4></li>
-          <?php
+          <li class="row header custom-pad-hor custom-pad-vert-small">
+            <span class="custom-pad-hor-space">Filter:</span> 
+            <?php
+              $filter1 = "";
+              if(!empty($_GET['filter1'])){
+                $filter1 = $_GET['filter1'];
+              }
+            ?>
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter1=".$filter1?>">All</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter1=".$filter1?>&filter2=poll">Poll</a>/
+            <a class="disinline custom-pad-hor-space" href="user.php?id=<?php echo $uscid; echo "&filter1=".$filter1?>&filter2=rating">Rating</a>
+          <?php 
+            if(!empty($_GET['filter2'])){
+              if(strcmp($_GET['filter2'], "poll") == 0){
+                $sql = $conn->prepare("SELECT * FROM survey WHERE user_id = ? AND survey_id LIKE \"P%\";");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans2 = $sql->get_result();
+              }else if(strcmp($_GET['filter2'], "rating") == 0){
+                $sql = $conn->prepare("SELECT * FROM survey WHERE user_id = ? AND survey_id LIKE \"R%\";");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans2 = $sql->get_result();
+              }else{
+                $sql = $conn->prepare("SELECT * FROM survey WHERE user_id = ?;");
+                $sql->bind_param('s', $uscid);
+                $sql->execute();
+                $ans2 = $sql->get_result();
+              }
+            }else{
+              $sql = $conn->prepare("SELECT * FROM survey WHERE user_id = ?;");
+              $sql->bind_param('s', $uscid);
+              $sql->execute();
+              $ans2 = $sql->get_result();
+            }
+
             for($i = 0; $i < $ans2->num_rows; $i++){
               $srow = $ans2->fetch_assoc();
               include('result-card.php');
